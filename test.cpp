5ed8d0f9e392ba3d481a2bfac4b8c0689e4d60f4
luistung <dongliang1986@gmail.com>
2022-11-16 01:04:09 +0800

    support emulate selects can be executed out of order

--- test.cpp
+++ test.cpp
@@ -64,61 +64,79 @@ TestCase sampleTestCase (std::mt19937& engine, const std::vector<Channel::Chan*>
     return ret;
 }
 
-void doEmulate(const TestCase& selectInstances, 
-                int pos,
-                set<const SelectInstance*>& cur, //alive select
-                set<set<const SelectInstance*>>& res
-                ) {
-    if (pos == selectInstances.size()) {
-        res.insert(cur);
-        return;
-    }
-    map<pair<Channel::METHOD, Channel::Chan *>, vector<const SelectInstance *>> wait2selectIns;
-    for (const SelectInstance* pSelectIns : cur) {
-        for (auto& [_, method, pChan, __] : pSelectIns->second) {
-            wait2selectIns[make_pair(method, pChan)].push_back(pSelectIns);
-        }
-    }
-
-    const SelectInstance* pSelectInstance = &selectInstances[pos].second;
-    bool found = false;
-    for (auto &[_, method, pChan, __] : pSelectInstance->second)
-    {
-        Channel::METHOD needMethod = (method == Channel::METHOD::READ ? Channel::METHOD::WRITE : Channel::METHOD::READ);
-        auto key = make_pair(needMethod, pChan);
-        if (wait2selectIns.find(key) != wait2selectIns.end()) {
-            found = true;
-            for (const SelectInstance* pMatchedSelectInstance : wait2selectIns.at(key)) {
-                cur.erase(pMatchedSelectInstance);
-                doEmulate(selectInstances, pos + 1, cur, res);
-                cur.insert(pMatchedSelectInstance);
-            }
-        }
-    }
-    if (!found) {
-        cur.insert(pSelectInstance);
-        doEmulate(selectInstances, pos + 1, cur, res);
-        cur.erase(pSelectInstance);
-    }
+pair<const  SelectInstance *, const SelectInstance *> makeOrderedSelectPair(const SelectInstance *a, const SelectInstance *b) {
+    return a < b ? make_pair(a, b) : make_pair(b, a);
 }
-
-set<Channel::NamedStatus> emulate(const TestCase& selectInstances) {
-    set<const SelectInstance *> cur;
-    set<set<const SelectInstance *>> res;
-    doEmulate(selectInstances, 0, cur, res);
-
-    set<Channel::NamedStatus> ret;
-    for (const set<const SelectInstance *> &selectInstanceSet : res) {
+void recursiveEmulate(vector<pair<pair<const SelectInstance *, const SelectInstance *>, bool>>& pSelectPairVec, 
+                      map<const SelectInstance*, bool>& usedSelect, 
+                      int pos, 
+                      set<Channel::NamedStatus>& res) {
+    if (pos >= pSelectPairVec.size()) {
+        for (pair<pair<const SelectInstance *, const SelectInstance *>, bool>& i : pSelectPairVec) {
+            if (i.second)
+                continue;
+            if (!usedSelect[i.first.first] && !usedSelect[i.first.second])
+                return;
+        }
         Channel::NamedStatus status;
-        for (const SelectInstance *pSelectInstance : selectInstanceSet) {
-            string selectName = pSelectInstance->first;
-            for (auto &[_, method, pChan, __] : pSelectInstance->second) {
+        for (auto& [pSelect, flag] : usedSelect) {
+            if (flag)
+                continue;
+            string selectName = pSelect->first;
+            for (auto &[_, method, pChan, __] : pSelect->second) {
                 status.insert(make_tuple(selectName, method, pChan->getName()));
             }
         }
-        ret.insert(status);
+        res.insert(status);
+        return;
     }
-    return ret;
+    recursiveEmulate(pSelectPairVec, usedSelect, pos + 1, res);
+    pair<pair<const SelectInstance *, const SelectInstance *>, bool>& pSelect = pSelectPairVec.at(pos);
+    const SelectInstance * pSelectA = pSelect.first.first;
+    const SelectInstance * pSelectB = pSelect.first.second;
+    if (!usedSelect[pSelectA] && !usedSelect[pSelectB]) {
+        usedSelect[pSelectA] = true;
+        usedSelect[pSelectB] = true;
+        pSelect.second = true;
+        recursiveEmulate(pSelectPairVec, usedSelect, pos + 1, res);
+        usedSelect[pSelectA] = false;
+        usedSelect[pSelectB] = false;
+        pSelect.second = false;
+    }
+}
+set<Channel::NamedStatus> emulate(const TestCase &selectInstances)
+{
+    map<pair<Channel::Chan *, Channel::METHOD>, vector<const SelectInstance*>> chanMethod2Select;
+    map<const SelectInstance*, bool> usedSelect;
+
+    for (auto& selectInstance : selectInstances)
+    {
+        const SelectInstance* pSelectIns = &selectInstance.second;
+        usedSelect[pSelectIns] = false;
+        for (auto &[_, method, pChan, __] : pSelectIns->second)
+        {
+            chanMethod2Select[make_pair(pChan, method)].emplace_back(pSelectIns);
+        }
+    }
+
+    set<pair<const SelectInstance *, const SelectInstance *>> pSelectPairSet;
+    for (auto &[pair_, selectVecA] : chanMethod2Select)
+    {
+        auto methodB = (pair_.second == Channel::METHOD::READ) ? Channel::WRITE : Channel::READ;
+        auto &selectVecB = chanMethod2Select[make_pair(pair_.first, methodB)];
+        for (const SelectInstance* pSelectA : selectVecA) {
+            for (const SelectInstance* pSelectB : selectVecB) {
+                pSelectPairSet.insert(makeOrderedSelectPair(pSelectA, pSelectB));
+            }
+        }
+    }
+
+    vector<pair<pair<const SelectInstance *, const SelectInstance *>, bool>> pSelectPairVec;
+    transform(pSelectPairSet.begin(), pSelectPairSet.end(), back_insert_iterator(pSelectPairVec), [](const auto &a)
+              { return make_pair(a, false); });
+    set<Channel::NamedStatus> res;
+    recursiveEmulate(pSelectPairVec, usedSelect, 0, res);
+    return res;
 }
 
 vector<Channel::NamedStatus> getInitNameStatus(TestCase& selectInstances) {
@@ -140,7 +158,7 @@ auto taskFun = [](const microseconds &sleepTime, Channel::METHOD method) -> Chan
     auto retFun = [=](const std::string &selectName, const std::string &chanName,
                      int a)
     {
-        //this_thread::sleep_for(sleepTime);
+        this_thread::sleep_for(sleepTime);
         log("###%s:%s:%s:%d\n", selectName.c_str(), ((method == Channel::METHOD::READ) ? "read" : "write"), chanName.c_str(), a);
         return true;
     };
@@ -200,6 +218,7 @@ void testcase(std::mt19937& engine) {
 }
 int main() {
     for (int i = 0; i < 100; i++) {
+        std::cout << "rand number:" << i << std::endl;
         std::mt19937 engine(i);
         testcase(engine);
     }
